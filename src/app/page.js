0d6c0b6948d2d92cd201038651983113a393624d
Jack G <jackrgisel@gmail.com>
2023-03-23 21:05:17 -0700

    stuff

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,53 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const apiKey = process.env.WEATHER_KEY;
 
 export default function Home() {
+  const [willRain, setWillRain] = useState(false);
+
+  useEffect(() => {
+    // Get current location
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      // Make API request to get weather forecast for current location
+
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          // Parse weather forecast data and check if it's going to rain in the next 2 weeks
+          const forecastList = data.list;
+          const twoWeeksForecast = forecastList.slice(0, 14);
+          const isRainExpected = twoWeeksForecast.some((forecast) => {
+            return forecast.weather.some((weather) => weather.main === "Rain");
+          });
+
+          if (isRainExpected) {
+            console.log("It is going to rain in the next 2 weeks!");
+            setWillRain(true);
+          } else {
+            console.log("No rain is expected in the next 2 weeks.");
+            setWillRain(false);
+          }
+        })
+        .catch((error) => {
+          console.error("Error getting weather forecast:", error);
+        });
+    });
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className={inter.className}>{willRain ? "No" : "Yes"}</p>
     </main>
-  )
+  );
 }
