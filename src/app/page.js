729426153358158bda760220ca2ed194410835d3
Jack G <jackrgisel@gmail.com>
2023-03-24 18:43:11 -0700

    fix weather issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,47 @@
-"use client";
-
 import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
 const apiKey = process.env.WEATHER_KEY;
 
-export default function Home() {
-  const [willRain, setWillRain] = useState(false);
+async function getForecast() {
+  const res = await fetch("https://ipapi.co/json/");
+  const data = await res.json();
 
-  useEffect(() => {
-    // Get current location
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+  const { latitude, longitude } = data;
 
-      // Make API request to get weather forecast for current location
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
-      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  return fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Parse weather forecast data and check if it's going to rain in the next 2 weeks
+      const forecastList = data.list;
+      const twoWeeksForecast = forecastList.slice(0, 14);
+      const isRainExpected = twoWeeksForecast.some((forecast) => {
+        return forecast.weather.some((weather) =>
+          ["Rain", "Snow"].includes(weather.main)
+        );
+      });
 
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          // Parse weather forecast data and check if it's going to rain in the next 2 weeks
-          const forecastList = data.list;
-          const twoWeeksForecast = forecastList.slice(0, 14);
-          const isRainExpected = twoWeeksForecast.some((forecast) => {
-            return forecast.weather.some((weather) => weather.main === "Rain");
-          });
-
-          if (isRainExpected) {
-            console.log("It is going to rain in the next 2 weeks!");
-            setWillRain(true);
-          } else {
-            console.log("No rain is expected in the next 2 weeks.");
-            setWillRain(false);
-          }
-        })
-        .catch((error) => {
-          console.error("Error getting weather forecast:", error);
-        });
+      return isRainExpected;
+    })
+    .catch((error) => {
+      console.error("Error getting weather forecast:", error);
+      return undefined;
     });
-  }, []);
+}
+
+export default async function Page() {
+  const isRainExpected = await getForecast();
 
   return (
     <main className={styles.main}>
-      <p className={inter.className}>{willRain ? "No" : "Yes"}</p>
+      {isRainExpected === undefined ? (
+        <p>could not get weather</p>
+      ) : (
+        <p className={inter.className}>{isRainExpected ? "No" : "Yes"}</p>
+      )}
     </main>
   );
 }
